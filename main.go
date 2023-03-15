2e47f8006307e2a42d2d69c45ab6f852235e2deb
ddzizz <ding_r@foxmail.com>
2023-03-15 17:02:38 +0800

    use gin insteda fiber

--- main.go
+++ main.go
@@ -2,27 +2,23 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v3"
 	"io/ioutil"
 	"log"
-	"time"
-
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/favicon"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/gofiber/template/html"
-	"gopkg.in/yaml.v3"
+	"net/http"
 )
 
 type Config struct {
-	Port         string `yaml:"port"`
+	Port    string            `yaml:"port"`
 	Buckets map[string]string `yaml:"buckets"`
-	Debug        bool   `yaml:"debug"`
+	Debug   bool              `yaml:"debug"`
 }
 
 func main() {
 	defaultCfg := Config{
-		Port:         "3000",
-		Debug:        false,
+		Port:  "3000",
+		Debug: false,
 	}
 	cfg, err := LoadConfig("cfg.yaml")
 	if err != nil {
@@ -32,80 +28,36 @@ func main() {
 		cfg = &defaultCfg
 	}
 
-	engine := html.New("./views", ".html")
-
-	// Fiber instance
-	app := fiber.New(fiber.Config{
-		Views: engine,
-	})
+	app := gin.Default()
 
 	// Routes
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendFile("./static/html/welcome.html")
+	app.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "./static/html/welcome.html", nil)
 	})
 
-	// app.Get("/s/:key/*", func(c *fiber.Ctx) error {
-	// 	key, err := url.PathUnescape(c.Params("key"))
-	// 	if err != nil {
-	// 		return c.Status(fiber.StatusNotFound).SendFile("./static/html/404.html")
-	// 	}
-	//
-	// 	params, err := url.PathUnescape(c.Params("*"))
-	// 	if err != nil {
-	// 		return c.Status(fiber.StatusNotFound).SendFile("./static/html/404.html")
-	// 	}
-	//
-	// 	bucket, ok := cfg.Buckets[key]
-	// 	if !ok {
-	// 		return c.Status(fiber.StatusNotFound).SendFile("./static/html/404.html")
-	// 	}
-	//
-	// 	objPath := path.Join(bucket, params)
-	// 	
-	// 	if cfg.Debug == true {
-	// 		return c.SendString(objPath)
-	// 	}
-	// 	return c.SendFile(objPath)
-	// })
-
 	for k, b := range cfg.Buckets {
-		app.Static("/s/" + k, b)
+		app.Static("/s/"+k, b)
 	}
 
 	// app.Static("/assets", "./assets")
 	app.Static("/static", "./static")
-	app.Use(logger.New(logger.Config{
-		Format: "${time} ${status} - ${latency} ${method} ${path}\n",
-		// TimeFormat: "02-Jan-2006",
-		TimeFormat: time.RFC3339Nano,
-		TimeZone:   "Asia/Shanghai",
-	}))
-	app.Use(favicon.New(favicon.Config{
-		File: "./static/icon/favicon.ico",
-		URL:  "/favicon.ico",
-	}))
-
-	app.Use(func(c *fiber.Ctx) error {
-		return c.Status(fiber.StatusNotFound).SendFile("./static/html/404.html")
-	})
-
-	// app.Static("/storage", "./storage/")
-
-	// app.Get("/versions/api", controllers.VersionsGetAll)
-	// app.Post("/versions/api", controllers.VersionsInsert)
-	// app.Get("/versions/api/:version", controllers.VersionsFind)
-	// app.Put("/versions/api/:version", controllers.VersionsUpdate)
-	// app.Delete("/versions/api/:version", controllers.VersionsDelete)
+	// app.Use(logger.New(logger.Config{
+	// 	Format: "${time} ${status} - ${latency} ${method} ${path}\n",
+	// 	// TimeFormat: "02-Jan-2006",
+	// 	TimeFormat: time.RFC3339Nano,
+	// 	TimeZone:   "Asia/Shanghai",
+	// }))
+	// app.Use(favicon.New(favicon.Config{
+	// 	File: "./static/icon/favicon.ico",
+	// 	URL:  "/favicon.ico",
+	// }))
 	//
-	// app.Get("/versions/view", controllers.VersionsViewGetAll)
-	// app.Get("/versions/view/edit", controllers.VersionsViewEditFind)
-	// app.Post("/versions/view/edit", controllers.VersionsViewEditInsert)
-	// app.Get("/versions/view/edit/:version", controllers.VersionsViewEditFind)
-	// app.Put("/versions/view/edit/:version", controllers.VersionsViewEditUpdate)
-	// app.Get("/versions/:id/page", controllers.VersionsGetAll)
-
+	// app.Use(func(c *fiber.Ctx) error {
+	// 	return c.Status(fiber.StatusNotFound).SendFile("./static/html/404.html")
+	// })
+	//
 	// Start server
-	log.Fatal(app.Listen(":" + cfg.Port))
+	log.Fatal(app.Run(":" + cfg.Port))
 }
 
 func LoadConfig(path string) (*Config, error) {
